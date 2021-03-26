ae0cc6621189a932a746f5ade9c1e3cf45e7cf5f
Ajay Rathod <theajayrathod05@gmail.com>
2021-03-26 14:03:59 +0530

    add env file and migrate table

--- resources/views/admin/layouts/main_sidebar.blade.php
+++ resources/views/admin/layouts/main_sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="{{url('admin/dashboard')}}" class="brand-link">
       <img src="{{asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Tecnomatrix</span>
+      <span class="brand-text font-weight-light">Technomatrix</span>
     </a>
 
     <!-- Sidebar -->
